c6b65c7bb7d8c597df74eea6f77f1fbcf34eea62
YuanZezhong <m15901952381@163.com>
2017-08-10 14:34:34 +0800

    增加SharedPreferences功能

---
+++ applib/src/main/java/cn/sh/changxing/yuanyi/preference/IPreferences.java
@@ -0,0 +1,14 @@
+package cn.sh.changxing.yuanyi.preference;
+
+import java.util.Map;
+
+/**
+ * Created by YuanZezhong on 2017/8/10.
+ *
+ * 定义Preference的功能
+ */
+public interface IPreferences {
+    public <T> IPreferences put(String key, T value);
+
+    public <T> IPreferences putMap(Map<String, T> map);
+}
